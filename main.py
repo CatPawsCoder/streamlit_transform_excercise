30ff331f06d89f16a853efea5c2e40841150740b
Meme <dariabespal@gmail.com>
2024-06-11 22:25:03 +0300

    formatted code with black

--- main.py
+++ main.py
@@ -9,19 +9,17 @@ from transformers import SegformerImageProcessor, SegformerForSemanticSegmentati
 import matplotlib.pyplot as plt
 
 # Load models
-device = (
-    "cuda"
-    if torch.cuda.is_available()
-    else "cpu"
-)
+device = "cuda" if torch.cuda.is_available() else "cpu"
 image_processor = SegformerImageProcessor.from_pretrained("jonathandinu/face-parsing")
 model = SegformerForSemanticSegmentation.from_pretrained("jonathandinu/face-parsing")
 model.to(device)
+
 
 # Function to preprocess the image
 def preprocess_image(img):
     img = img.resize((512, 512))  # Resize image to match model input size
     return img
+
 
 # Function to run inference and visualize results
 def process_and_display(image):
@@ -29,30 +27,30 @@ def process_and_display(image):
     inputs = image_processor(images=image, return_tensors="pt").to(device)
     outputs = model(**inputs)
     logits = outputs.logits
-    upsampled_logits = nn.functional.interpolate(logits,
-                                                  size=image.size[::-1], 
-                                                  mode='bilinear',
-                                                  align_corners=False)
+    upsampled_logits = nn.functional.interpolate(
+        logits, size=image.size[::-1], mode="bilinear", align_corners=False
+    )
     labels = upsampled_logits.argmax(dim=1)[0].cpu().numpy()
 
-      # Visualize results
+    # Visualize results
     fig, ax = plt.subplots()
     ax.imshow(labels)
-    ax.axis('off')
+    ax.axis("off")
     st.pyplot(fig)
 
-# Main Streamlit app
-st.title('Face Parsing App')
 
-uploaded_file = st.file_uploader(label='Upload an image', type=['jpg', 'png'])
+# Main Streamlit app
+st.title("Face Parsing App")
+
+uploaded_file = st.file_uploader(label="Upload an image", type=["jpg", "png"])
 
 if uploaded_file is not None:
     # Display the uploaded image
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
 
     # Preprocess and process the image
     preprocessed_image = preprocess_image(image)
-    result = st.button('Process Image')
+    result = st.button("Process Image")
     if result:
         process_and_display(preprocessed_image)
--- test_main.py
+++ test_main.py
@@ -3,14 +3,15 @@ from PIL import Image
 import pytest
 from main import preprocess_image, process_and_display
 
+
 @pytest.fixture
 def sample_image():
     # Create a sample image for testing
-    image = Image.new('RGB', (512, 512), color='white')
+    image = Image.new("RGB", (512, 512), color="white")
     return image
+
 
 def test_preprocess_image(sample_image):
     # Test preprocess_image function
     preprocessed_image = preprocess_image(sample_image)
     assert preprocessed_image.size == (512, 512)
-
