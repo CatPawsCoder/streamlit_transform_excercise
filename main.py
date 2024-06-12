f90d9c1222278b9c52c1df39df5bee5e29ecd7d3
Meme <dariabespal@gmail.com>
2024-06-12 16:10:59 +0300

    Add docstrings and comments to functions

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# main.py
+"""
+Face Parsing App
+This application allows users to upload a photo of a person and perform semantic segmentation of the face.
+"""
+
 import streamlit as st
 from PIL import Image
 import torch
@@ -11,16 +17,26 @@ image_processor = SegformerImageProcessor.from_pretrained("jonathandinu/face-par
 model = SegformerForSemanticSegmentation.from_pretrained("jonathandinu/face-parsing")
 model.to(device)
 
+def preprocess_image(img: Image.Image) -> Image.Image:
+    """
+    Resize the input image to match the model's input size.
 
-# Function to preprocess the image
-def preprocess_image(img):
-    img = img.resize((512, 512))  # Resize image to match model input size
+    Args:
+        img (Image.Image): Input image.
+
+    Returns:
+        Image.Image: Resized image.
+    """
+    img = img.resize((512, 512))
     return img
 
+def process_and_display(image: Image.Image):
+    """
+    Run inference on the input image and display the results.
 
-# Function to run inference and visualize results
-def process_and_display(image):
-    # Run inference
+    Args:
+        image (Image.Image): Input image.
+    """
     inputs = image_processor(images=image, return_tensors="pt").to(device)
     outputs = model(**inputs)
     logits = outputs.logits
@@ -29,25 +45,20 @@ def process_and_display(image):
     )
     labels = upsampled_logits.argmax(dim=1)[0].cpu().numpy()
 
-    # Visualize results
     fig, ax = plt.subplots()
     ax.imshow(labels)
     ax.axis("off")
     st.pyplot(fig)
 
-
-# Main Streamlit app
 st.title("Face Parsing App")
-
 uploaded_file = st.file_uploader(label="Upload an image", type=["jpg", "png"])
 
 if uploaded_file is not None:
-    # Display the uploaded image
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
-    # Preprocess and process the image
     preprocessed_image = preprocess_image(image)
     result = st.button("Process Image")
     if result:
         process_and_display(preprocessed_image)
+
